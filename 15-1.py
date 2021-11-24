508a139f97b993d5a7f90a9bc559d89f40e1f6d1
curtinorketing <94956422+curtinorketing@users.noreply.github.com>
2021-11-24 15:58:49 +0800

    add a fire

---
+++ 15-1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 13:59:00 2021
+
+@author: curtin
+"""
+#numpy.savetxt
+import numpy as np
+a=np.random.rand(5,6)
+print(a)
+
+np.savetxt('array.txt',a,fmt='%0.5f',delimiter=',')
